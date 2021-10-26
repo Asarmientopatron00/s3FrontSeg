ac2484bd4ca6fe67780d7c856dd93bca97a582bb
Daniel Rendon <drendonr3@gmail.com>
2021-10-26 00:19:36 -0500

    Correcciones Carga Masiva

--- src/@crema/utility/ContextProvider/defaultConfig.js
+++ src/@crema/utility/ContextProvider/defaultConfig.js
@@ -155,6 +155,6 @@ const defaultConfig = {
   rtlLocale: ['ar'],
   API_URL: environment.API_URL,
   // API_URL: 'https://solicitudesservicio-backt.sellosdeseguridad.net/public',
-  version: 'V 9.0.2',
+  version: 'V 9.0.3',
 };
 export default defaultConfig;
