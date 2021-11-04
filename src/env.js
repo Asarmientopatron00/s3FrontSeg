2f71afa385e4bea05adad602de9d9a6204a663d8
Daniel Rendon <drendonr3@gmail.com>
2021-11-04 00:15:57 -0500

    Arreglado Estados Pedidos

--- src/env.js
+++
@@ -1,6 +0,0 @@
-const environment = {
-  API_URL: 'http://solicitudesservicio.test',
-  // API_URL: 'https://solicitudesservicio-back.sellosdeseguridad.net/public',
-};
-
-export default environment;
